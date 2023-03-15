2512be872f0feb301527772c17033bd609ed9e11
emiclar <emiclar@aflac.com>
2023-03-14 22:41:21 -0400

    Added catch-all route and NotFoundView component to show a 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import EventListView from "../views/EventListView.vue";
 import EventDetailsView from "../views/EventDetailsView.vue";
 import AboutView from "../views/AboutView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,10 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       // component: () => import("../views/AboutView.vue"),
     },
+    { 
+      path: '/:pathMatch(.*)', 
+      component: NotFoundView, 
+    }
   ],
 });
 
